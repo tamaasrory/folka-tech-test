c127d2f2b99fd2324b9dc766f02be6b0e94a9356
tama <tama1234>
2022-01-03 12:04:29 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -54,9 +54,11 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior () {
+    document.getElementById('app').scrollIntoView()
+  }
 })
 
 router.beforeEach((to, from, next) => {
